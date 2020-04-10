bfdaebf8e47faa43ea46398aecfd538967da4f75
Marcos Marado <mindboosternoori@gmail.com>
2020-04-10 20:43:34 +0100

    py3: tweet.text doesn't need to be encoded anymore

--- tweet_dumper.py
+++ tweet_dumper.py
@@ -62,7 +62,7 @@ def get_all_tweets(screen_name):
 		print("...%s tweets downloaded so far" % (len(alltweets)))
 	
 	#transform the tweepy tweets into a 2D array that will populate the csv	
-	outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
+	outtweets = [[tweet.id_str, tweet.created_at, tweet.text] for tweet in alltweets]
 	
 	#write the csv	
 	with open('%s_tweets.csv' % screen_name, 'w') as f:
